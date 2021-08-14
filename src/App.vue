c3230fde88c49eaa23b01494216c4eba65d3f746
Vasile Milea <milea.vasile959@gmail.com>
2021-08-14 15:13:54 +0000

    Vue.js files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+		<columns-card></columns-card>
+	</div>
+</template>
+
+<script>
+import ColumnsCard from './components/ColumnsCard.vue'
+
+export default {
+	components: {
+		ColumnsCard,
+	},
+}
+</script>
